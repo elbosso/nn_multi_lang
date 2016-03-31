87161d611e914e55ac8fa3cc669b4784134956a8
elbosso <elbosso@tichy.fritz.box>
2016-03-31 17:19:25 +0200

    network finished

--- python/neuralgas.py
+++ python/neuralgas.py
@@ -1,5 +1,8 @@
 #import os,sys,inspect
 import math
+import nn
+import random
+import sys
 
 class Node(nn.FeatureRepresentation):
 	'doc'
@@ -8,7 +11,7 @@ class Node(nn.FeatureRepresentation):
 		nn.FeatureRepresentation.__init__(self,features);
 
 	def computeSquaredFeatureDistance(self, sample):
-		squaredFeaturesDistance=super(Node, self).computeSquaredFeatureDistance(sample)
+		squaredFeaturesDistance=nn.FeatureRepresentation.computeSquaredFeatureDistance(self,sample)
 		return squaredFeaturesDistance;
 
 	def getSquaredFeaturesDistance(self):
@@ -17,12 +20,61 @@ class Node(nn.FeatureRepresentation):
 	def updateFeatures(self, e,  lamb, sample, k):
 		h=math.exp(-k/lamb)
 		for i in range(0,len(sample)):
-			features[i]+=e*h*(sample[i]-features[i])
+			self.features[i]+=e*h*(sample[i]-self.features[i])
 
-gen=nn.Generator()
-features=gen.generateSample(2)
-sample=gen.generateSample(2)
-a=Node(features);
-print a
-a.updateFeatures(0.6,5,sample,3)
-print a
+class Generator(nn.Generator):
+    'doc'
+        
+    def generate(self,nodeCount,dimensionOfFeatures):
+        nodes=range(nodeCount);
+        for j in nodes:
+            features=range(dimensionOfFeatures)
+            for i in features:
+                features[i]=random.random()
+            nodes[j]=Node(features)
+        return nodes;
+
+class Network:
+    'doc'
+    
+    def __init__(self,nodes):
+        self.nodes=nodes;
+        
+    def init(self,tmax,estart,eend,lambdastart,lambdaend):
+        self.t=0;
+        self.tmax=tmax
+        self.estart=estart
+        self.eend=eend;
+        self.lambdastart=lambdastart;
+        self.lambdaend=lambdaend;
+
+    def getT(self):
+        return self.t;
+        
+    def learn(self, sample):
+        featureDistMin=sys.float_info.max
+        for node in self.nodes:
+            dist=node.computeSquaredFeatureDistance(sample)
+            if dist<featureDistMin:
+                winner=node
+                featureDistMin=dist
+        timeq=self.t/self.tmax;
+        e=self.estart*(pow(self.eend/self.estart, timeq));
+        lambd=self.lambdastart*(pow(self.lambdaend/self.lambdastart, timeq));
+        i=0;
+        for node in self.nodes:
+            node.updateFeatures(e, lambd,sample,i);
+            ++i
+        self.t+=1;
+
+gen=Generator()
+nodes=gen.generate(25,2)
+network=Network(nodes)
+network.init(10000,1.0,0.001,13,0.01)
+i=0
+plotinterval=12000
+for i in range(0, 10000):
+    sample=gen.generateSample(2);
+    network.learn(sample);
+#    if (i+1)%plotinterval==0:
+#        SVGRenderer.outNodes("neuralgas",i+1,java.util.Arrays.asList(network.getNodes()));
